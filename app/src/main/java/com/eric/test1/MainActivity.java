fa317220fb52b4e61fd9ce9166373cfd9952ed2b
Eric <wilbur.h@tom.com>
2021-10-28 00:46:48 +0800

    bind buttons

--- app/src/main/java/com/eric/test1/MainActivity.java
+++ app/src/main/java/com/eric/test1/MainActivity.java
@@ -2,9 +2,12 @@ package com.eric.test1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +17,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button h2p1 = (Button) findViewById(R.id.btn_hw2p1);
+        h2p1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, hw2p1.class);
+                startActivity(intent);
+            }
+        });
+
+        Button h2p2 = (Button) findViewById(R.id.btn_hw2p1);
+        h2p2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, hw2p2.class);
+                startActivity(intent);
+            }
+        });
+
+        Button h2p3 = (Button) findViewById(R.id.btn_hw2p1);
+        h2p3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, hw2p3.class);
+                startActivity(intent);
+            }
+        });
+
 
     }
+
 }
